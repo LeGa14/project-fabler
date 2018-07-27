6b789f3272e867c680916b1afc95f29cb9f87de1
LeGa14 <glemonjr68@gmail.com>
2018-07-27 15:52:01 -0400

    routes specified

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,60 @@
 Rails.application.routes.draw do
+
+  # props routes thru settings
   namespace :api do
-    get 'props/index'
-    get 'props/show'
+    namespace :settings do
+      get 'props/index'
+      get 'props/show'
+      post 'props/create'
+      patch 'props/update'
+      delete 'props/destroy'
+    end
   end
+# props routes thru chars
+  namespace :api do
+    namespace :characters do
+      get 'props/index'
+      get 'props/show'
+      post 'props/create'
+      patch 'props/update'
+      delete 'props/destroy'
+    end
+  end
+
+  namespace :api do
+    get 'settings/index'
+    get 'settings/show'
+    post 'settings/create'
+    patch 'settings/update'
+    delete 'settings/destroy'
+  end
+
+  namespace :api do
+    get 'characters/index'
+    get 'characters/show'
+    post 'characters/create'
+    patch 'characters/update'
+    delete 'characters/destroy'
+  end
+
+  namespace :api do
+    get 'actions/index'
+    get 'actions/show'
+    post 'actions/create'
+    patch 'actions/update'
+    delete 'actions/destroy'
+  end
+  
   namespace :api do
     resources :characters do
       resources :props
     end
+  end
+
+  namespace :api do
     resources :settings do
       resources :props
     end
-    resources :actions
   end
+
 end

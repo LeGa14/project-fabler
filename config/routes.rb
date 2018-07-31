1f2c4b3320f651ecd267590a855637f2bf8b86e3
LeGa14 <glemonjr68@gmail.com>
2018-07-30 20:29:28 -0400

    routes structured for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,125 @@
 Rails.application.routes.draw do
 
-  # props routes thru settings
   namespace :api do
-    namespace :settings do
-      get 'props/index'
-      get 'props/show'
-      post 'props/create'
-      patch 'props/update'
-      delete 'props/destroy'
-    end
-  end
-# props routes thru chars
-  namespace :api do
-    namespace :characters do
-      get 'props/index'
-      get 'props/show'
-      post 'props/create'
-      patch 'props/update'
-      delete 'props/destroy'
+    namespace :stories do
+      namespace :scenarios do
+        namespace :settings do
+          get 'props/index'
+          get 'props/show'
+          get 'props/create'
+          get 'props/update'
+          get 'props/destroy'
+        end
+      end 
     end
   end
 
   namespace :api do
-    get 'settings/index'
-    get 'settings/show'
-    post 'settings/create'
-    patch 'settings/update'
-    delete 'settings/destroy'
-  end
-
-  namespace :api do
-    get 'characters/index'
-    get 'characters/show'
-    post 'characters/create'
-    patch 'characters/update'
-    delete 'characters/destroy'
-  end
-
-  namespace :api do
-    get 'actions/index'
-    get 'actions/show'
-    post 'actions/create'
-    patch 'actions/update'
-    delete 'actions/destroy'
-  end
-  
-  namespace :api do
-    resources :characters do
-      resources :props
+    namespace :stories do
+      namespace :scenarios do
+        namespace :characters do
+          get 'props/index'
+          get 'props/show'
+          get 'props/create'
+          get 'props/update'
+          get 'props/destroy'
+        end
+      end 
     end
   end
 
   namespace :api do
-    resources :settings do
-      resources :props
+    namespace :stories do
+      namespace :scenarios do
+        get 'characters/index'
+        get 'characters/show'
+        get 'characters/create'
+        get 'characters/update'
+        get 'characters/destroy'
+      end 
+    end
+  end
+
+  namespace :api do
+    namespace :stories do
+      namespace :scenarios do
+        get 'settings/index'
+        get 'settings/show'
+        get 'settings/create'
+        get 'settings/update'
+        get 'settings/destroy'
+      end 
+    end
+  end
+
+  namespace :api do
+    namespace :stories do
+      namespace :scenarios do
+        get 'events/index'
+        get 'events/show'
+        get 'events/create'
+        get 'events/update'
+        get 'events/destroy'
+      end 
+    end
+  end
+
+  namespace :api do
+    namespace :stories do
+      namespace :scenarios do
+        get 'happenings/index'
+        get 'happenings/show'
+        get 'happenings/create'
+        get 'happenings/update'
+        get 'happenings/destroy'
+      end 
+    end
+  end
+
+  namespace :api do
+    namespace :stories do
+      namespace :scenarios do
+        get 'existents/index'
+        get 'existents/show'
+        get 'existents/create'
+        get 'existents/update'
+        get 'existents/destroy'
+      end 
+    end
+  end
+
+  namespace :api do
+    namespace :stories do
+      get 'scenarios/index'
+      get 'scenarios/show'
+      get 'scenarios/create'
+      get 'scenarios/update'
+      get 'scenarios/destroy'
+    end
+  end
+
+  namespace :api do
+      get 'stories/index'
+      get 'stories/show'
+      get 'stories/create'
+      get 'stories/update'
+      get 'stories/destroy'
+  end
+
+  namespace :api do
+    resources :stories do
+      resources :scenarios do
+        resources :existents
+        resources :settings do
+          resources :props
+        end
+        resources :characters do
+          resources :props
+        end
+        resources :events
+        resources :happenings
+        
+      end
     end
   end
 

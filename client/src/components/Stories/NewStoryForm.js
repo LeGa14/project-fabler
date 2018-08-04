b5d4da3460deb384a769de3ece408b9eef74a22b
LeGa14 <glemonjr68@gmail.com>
2018-08-04 00:10:18 -0400

    New Story Component created

---
+++ client/src/components/Stories/NewStoryForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewStoryForm extends Component {
+    render() {
+        return (
+            <div>
+                <h4>New Post Form</h4>
+            </div>
+        );
+    }
+}
+
+export default NewStory;
